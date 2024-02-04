b49f3985269181cd81f516b3588f81e780786575
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-02-04 22:39:43 +0530

    adding the app.js code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Design from "./Components/Design";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Home from "./views/home";
@@ -7,12 +7,15 @@ import Profile from "./views/profile";
 
 function App() {
   return (
-    <>
-       <Navbar/> 
-       <ProductRowIst/>
-       <Design/>
-      {/* <Footer/>  */}
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
